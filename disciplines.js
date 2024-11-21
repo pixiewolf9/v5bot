b4c8345a36e3afae5967547bc854f0a55d665e52
pixiewolf9 <47984832+pixiewolf9@users.noreply.github.com>
2024-11-20 23:30:33 -0500

    aving so i dint lose date

--- disciplines.js
+++ disciplines.js
@@ -1,1 +1,3 @@
 discipline["Animalism","Auspex","Blood Sorcery","Celerity","Dominate","Fortitude","Obfuscate","Oblivion","Potence","Presence","Presence","Thin-blood Alchemy"];
+//animalism discipline power level and name
+animalism["1:Bond Famulus","1:Sense the Beast"];
