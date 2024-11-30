d9531754ce77e43769e48bd14a3ed33e0e61e0c8
pixiewolf9 <47984832+pixiewolf9@users.noreply.github.com>
2024-11-30 16:14:18 -0500

    ls clan

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import discord
 import os
+client =discord.Client()
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.conent.startwith('/ls clan'):
+        await message.channel.send('Banu Haqim Brujah Gangrel Hecata Lasombra Malkavian The Ministry Nosferatu Ravnos Salubri Toreador Tremere Tzimisce Ventrue Caitiff Thin-blood')
+
